3512a2013ab3f2afacb7105d4ec26358492a5d91
Simon Ramstedt <simonramstedt@gmail.com>
2026-01-18 18:41:53 +0100

    fix(test)

--- test/__jix__.js
+++ test/__jix__.js
@@ -1,15 +1,32 @@
-import container from "./container.js"
+
+let PROJECT_ROOT = `${import.meta.dirname}/..`
+
+let image = () => jix.container.imageFromDockerfile`
+	FROM docker.io/nixos/nix@\
+	sha256:04abdb9c74e0bd20913ca84e4704419af31e49e901cd57253ed8f9762def28fd
+
+	RUN nix-env -iA nixpkgs.gcc
+	RUN nix-env -iA nixpkgs.gnumake
+	RUN nix-env -iA nixpkgs.gnupatch
+	RUN nix-env -iA nixpkgs.nodejs_22
+	RUN nix-env -iA nixpkgs.coreutils nixpkgs.findutils nixpkgs.gnugrep nixpkgs.gnused nixpkgs.gnutar nixpkgs.gzip nixpkgs.gawk
+	RUN nix-env -iA nixpkgs.curl
+
+	`
+
+let containerRun = () => jix.container.run({image, workdir: "/wd", volumes: {wd: PROJECT_ROOT}})
 
 export const run = {
 	containerized: jix.script`
-		cd ${import.meta.dirname}/..
-		${container.run()} bash -c '
+		${containerRun} bash -c '
+			make build
 			node --test --experimental-test-isolation=none test/*.test.js test/**/*.test.js
 		'
 	`,
 
 	default: jix.script`
 		cd ${import.meta.dirname}/..
+		make build
 		node --test --experimental-test-isolation=none test/*.test.js test/**/*.test.js
 	`,
 }
